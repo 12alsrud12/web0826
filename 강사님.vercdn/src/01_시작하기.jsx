2d0afa474ea3272eb1c2a27bac47c26a0bf2b461
12alsrud12 <12alsrud12@naver.com>
2024-08-30 09:32:16 +0900

    cdn학습

--- 강사님.vercdn/src/01_시작하기.jsx
+++ 강사님.vercdn/src/01_시작하기.jsx
@@ -1,28 +1,28 @@
 /*  
     1. 컴포넌트 형식
       function 컴포넌트(){
-        return (JSX 문장);
+        return (
+            JSX 문장
+            <hr>
+        );
       }
 
-    가. 컴포넌트 이름의 첫 글자는 대문자로 쓴다.
-    나. JSX의 태그 끝에는 ; 을 쓰지 않는다. 
-        나. 예) 위의 형식 부분에 (JSX 문장;) <- X, (JSX 문장) <- 이게 올바른 방식
-    다. JSX 문장이 한 문장일 경우 (); 생략 가능
-    라. JSX 문장은 "" 묶으면 안된다.
-    마. JSX 문장이 한 문장일 경우 (); 생략 가능
+      가. 컴포넌트 이름의 첫 글자는 대문자로 쓴다.
+      나. return 키워드 필수!
+      다. JSX의 태그 끝에는 ; 을 쓰지 않는다.
+      라. JSX 문장은 "" 묶으면 안된다.
+      마. JSX 문장이 한 문장일 경우 (); 생략 가능
 */
 
-// JSX 컴포넌트 생성! 첫 글자 대문자로 사용! 생성자 함수X
-// Hello = 함.수.형! 컴포넌트!이다.
+// 컴포넌트 생성
 function Hello() {
     // 리턴 뒤에 한 줄 문장
-    // 위의 다. 부분일 시 만약 h1 한개밖에 없다면 return <h1>Hello World!</h1> 으로 종료할 수 있다.
+    // return <h1>Hello World!</h1>
     // 리턴 뒤에 두 줄 이상의 문장은 ( ); 로 묶고 문장 끝 ; 쓴다.
     return (
         // JSX의 태그 끝에는 ; 쓰지 않는다.
         // return 안의 root 태그는 1개 이어야 한다.
         <div className="card_list">
-            {/* JS 사용시 {} <- 이 괄호로 감싸주어야 한다. */}
             {/* XML에서 빈 요소는 끝에 <img /> */}
             <div className="card_img">
                 <img
@@ -62,28 +62,6 @@ function Hello() {
 //   );
 // }
 
-// 컴포넌트 생성
-// 한 줄 인 경우
-function Haaa() {
-    // 리턴 뒤에 한 줄 문장
-    return <h2>Hello World!</h2>
-}
-
-// 두 줄 이상인 경우
-function Haaa2() {
-    return (
-        /* div 이 안부터 리엑트 부분 */
-        <div className="ui-card">
-            <div className="card-img">
-                <img src="" alt="" />
-            </div>
-        </div>
-    )
-}
-// 라이브러리 이름: React, ReactDOM(가상돔), Babel(JSX번역)
-
-// 리액트의 출력
-// 객체.메서드
 /* 
     ReactDOM.render(요소, 컨테이너); 
       1. ReactDOM은 가상 DOM을 생성하는 객체
