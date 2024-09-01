1fd3f17467f19ecd6a7da59dbbec163cf562b1c9
12alsrud12 <12alsrud12@naver.com>
2024-09-01 20:06:34 +0900

    17-test연습

--- 17_test연습/src/react.jsx
+++ 17_test연습/src/react.jsx
@@ -1,12 +1,16 @@
 // 리엑트 로드 이벤트 필요 없음!
 // 리액트 컴포넌트 생성!
-function UiCard() {
+function UiCard1() {
     return (
         <div className="ui-card">
             <div className="ui-img">
+                <img src="../images/ui-card-img1.svg" alt="ui_카드_이미지1" />
             </div>
             <div className="ui-bx">
-
+                <h4>라온</h4>
+                <p>0<span>(0)</span></p>
+                <h6>부산 수영구 · 모든견종 · 무선인터넷 · 엘리베이터</h6>
+                <h5>100,000원</h5>
             </div>
         </div>
     );
@@ -15,10 +19,10 @@ function UiCard() {
 // 출력
 ReactDOM.render(
     <div className="ui-card-list">
-        <UiCard />
-        <UiCard />
-        <UiCard />
-        <UiCard />
+        <UiCard1 />
+        <UiCard1 />
+        <UiCard1 />
+        <UiCard1 />
     </div>,
-    document.querySelector("#ui-card")
+    document.querySelector("#ui")
 )
